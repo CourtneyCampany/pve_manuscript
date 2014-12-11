6d4713c34c32d249e22c8fd67e03a34d9f6cf63f
Courtney Campany <courtneycampany@gmail.com>
2014-12-11 17:17:58 +1100

    up to methods in manuscript

--- manuscript/r_packages_bibtex.R
+++ manuscript/r_packages_bibtex.R
@@ -7,3 +7,11 @@
         year = {2014},
         note = {R package version 0.2-3},
 }
+
+@Manual{nlme,
+        title = {{nlme}: Linear and Nonlinear Mixed Effects Models},
+        author = {Jose Pinheiro and Douglas Bates and Saikat DebRoy and Deepayan Sarkar and {R Core Team}},
+        year = {2014},
+        note = {R package version 3.1-118},
+        url = {http://CRAN.R-project.org/package=nlme},
+}
