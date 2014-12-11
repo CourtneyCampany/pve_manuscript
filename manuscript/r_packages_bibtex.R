cef30147b87cae6467f200b7be0abe904bf37016
Courtney Campany <courtneycampany@gmail.com>
2014-12-11 15:54:43 +1100

    started manuscript in latex

---
+++ manuscript/r_packages_bibtex.R
@@ -0,0 +1,9 @@
+###R packages bibtex to copy
+
+
+@Manual{plantecophys,
+        title = {plantecophys: Modelling and analysis of leaf gas exchange data},
+        author = {Remko Duursma},
+        year = {2014},
+        note = {R package version 0.2-3},
+}
